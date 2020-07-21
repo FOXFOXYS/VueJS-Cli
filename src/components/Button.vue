aaa69006ff3d7d653ac35060a5f9582ae652c960
FOXFOXYS <foxfoxy.pro@gmail.com>
2020-07-21 11:48:13 +0200

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,31 @@
+<template>
+   <button class="button button--red" @click="incrementCount">Cliqué {{count}} fois</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    data: function() {
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        incrementCount: function() {
+            this.count++;
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.button {
+    padding: 20px;
+    color: white;
+    background-color: blue;
+
+    &--red {
+        background-color: red;
+    }
+}
+</style>
